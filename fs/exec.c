7e0a95ef9ad84522fd47d71750b2554839d61ac0
missgoin <mister.goindo@gmail.com>
2023-06-06 13:04:43 +0700

    ksu stable

--- fs/exec.c
+++ fs/exec.c
@@ -1707,8 +1707,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 // KernelSU hook
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
 
 /*
  * sys_execve() executes a new program.
--- fs/open.c
+++ fs/open.c
@@ -355,8 +355,7 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 }
 
 // KernelSU hook
-extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-			        int *flags);
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *flags);
 
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
